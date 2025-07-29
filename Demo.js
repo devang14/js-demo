f2a4319d54ce94366823cac3e2d2303141dec500
devang raj <raj.devang482@gmail.com>
2025-07-29 08:30:54 +0530

    add new line in js

--- Demo.js
+++ Demo.js
@@ -1,5 +1,8 @@
 //console.log("This is my first example");
 // alert("I have started learning js");
 
- alert("Please check my investment")
+       alert("Please check my investment")
         console.log("My first investment is 50000Rs")
+
+       alert ("my opinion")
+       console.log("it doesn't matter for any other")
